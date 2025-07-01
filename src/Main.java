d963e3476d5bce65ef7953346a867b95ecb89057
Talic72 <EMORRISWORK72@GMAIL.COM>
2025-06-30 22:22:01 -0400

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            Scanner in = new Scanner(System.in);
+
+            double FEE_PRICE = .02;
+            int comparor = 100;
+
+            System.out.println("What is the price of your item:");
+            double beforeCost = in.nextDouble();
+
+            if(beforeCost >= comparor) {
+                System.out.println("Your final cost is $" + beforeCost + " since your item totals more then 100 no fee is applied.");
+            }
+            else {
+                double outCost = beforeCost + (beforeCost * FEE_PRICE);
+                System.out.println("Your final cost with the under 100 fee applied is $" + outCost);
+            }
+
     }
 }
